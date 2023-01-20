16d6d2189677c616c8eb584e2aa9ec4ddb879bff
Жданова Вика <vikazhdanova97@mail.ru>
2023-01-20 10:06:46 +0300

    second commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,20 +1,16 @@
 const links = document.querySelectorAll(".menu_main > li > a.menu__link");
-console.log(links);
 
 for (let i = 0; i < links.length; ++i) {
-    console.log(links[i]);
     links[i].onclick = function () {
-        console.log(this);
         if (this.parentElement.querySelectorAll("ul").length) {
             let isActive = this.parentElement.querySelector("ul").classList.contains('menu_active');
-            console.log(isActive);
             document.querySelectorAll(".menu_main ul.menu.menu_sub.menu_active").forEach(function (item) {
                 item.classList.remove("menu_active");
             });
-            if (!isActive) {
+            if (!isActive)
                 this.parentElement.querySelector("ul").classList.add("menu_active");
             return false;
-            }
         }
     };
+
 }
